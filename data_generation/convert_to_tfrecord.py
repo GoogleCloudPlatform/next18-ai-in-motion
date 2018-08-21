ff53046d574becc2f699dd26bf1ba111d475a9a8
Yu-Han Liu <yuhanliu@google.com>
2018-08-21 16:12:11 -0700

    update convert_to_tfrecord.py

--- data_generation/convert_to_tfrecord.py
+++ data_generation/convert_to_tfrecord.py
@@ -1,25 +1,25 @@
-# source: https://github.com/sararob/tswift-detection/blob/master/convert_to_tfrecord.py
-
-# Copyright 2017 Google LLC
-
+# Copyright 2018 The TensorFlow Authors. All Rights Reserved.
+#
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
-
-#     https://www.apache.org/licenses/LICENSE-2.0
-
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
 # Unless required by applicable law or agreed to in writing, software
 # distributed under the License is distributed on an "AS IS" BASIS,
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+# ==============================================================================
+
+# source: https://github.com/sararob/tswift-detection/blob/master/convert_to_tfrecord.py
 
 import os
 import io
 import xml.etree.ElementTree as ET
 import tensorflow as tf
 
-from object_detection.utils import dataset_util
 from PIL import Image
 
 
@@ -29,11 +29,27 @@ flags.DEFINE_string('images_dir', '', 'Path to directory of images')
 flags.DEFINE_string('labels_dir', '', 'Path to directory of labels')
 FLAGS = flags.FLAGS
 
+# helper functions
+# https://github.com/tensorflow/models/blob/master/research/object_detection/utils/dataset_util.py
+def int64_feature(value):
+    return tf.train.Feature(int64_list=tf.train.Int64List(value=[value]))
+
+def int64_list_feature(value):
+    return tf.train.Feature(int64_list=tf.train.Int64List(value=value))
+
+def bytes_feature(value):
+    return tf.train.Feature(bytes_list=tf.train.BytesList(value=[value]))
+
+def bytes_list_feature(value):
+    return tf.train.Feature(bytes_list=tf.train.BytesList(value=value))
+
+def float_list_feature(value):
+    return tf.train.Feature(float_list=tf.train.FloatList(value=value))
+
 
 def create_tf_example(example):
-
-    image_path = os.getcwd() + '/' +  FLAGS.images_dir + example
-    labels_path = os.getcwd() + '/' +  FLAGS.labels_dir + os.path.splitext(example)[0] + '.xml'
+    image_path = os.path.join(FLAGS.images_dir, example)
+    labels_path = os.path.join(FLAGS.labels_dir, os.path.splitext(example)[0] + '.xml')
 
     # Read the image
     img = Image.open(image_path)
@@ -69,18 +85,18 @@ def create_tf_example(example):
         classes.append(int(class_.find('label').text))
 
     tf_example = tf.train.Example(features=tf.train.Features(feature={
-        'image/height': dataset_util.int64_feature(height),
-        'image/width': dataset_util.int64_feature(width),
-        'image/filename': dataset_util.bytes_feature(example.encode('utf-8')),
-        'image/source_id': dataset_util.bytes_feature('overlay.py'.encode('utf-8')),
-        'image/encoded': dataset_util.bytes_feature(encoded_image_data),
-        'image/format': dataset_util.bytes_feature(image_format),
-        'image/object/bbox/xmin': dataset_util.float_list_feature(xmins),
-        'image/object/bbox/xmax': dataset_util.float_list_feature(xmaxs),
-        'image/object/bbox/ymin': dataset_util.float_list_feature(ymins),
-        'image/object/bbox/ymax': dataset_util.float_list_feature(ymaxs),
-        'image/object/class/text': dataset_util.bytes_list_feature(classes_text),
-        'image/object/class/label': dataset_util.int64_list_feature(classes),
+        'image/height': int64_feature(height),
+        'image/width': int64_feature(width),
+        'image/filename': bytes_feature(example.encode('utf-8')),
+        'image/source_id': bytes_feature('overlay.py'.encode('utf-8')),
+        'image/encoded': bytes_feature(encoded_image_data),
+        'image/format': bytes_feature(image_format),
+        'image/object/bbox/xmin': float_list_feature(xmins),
+        'image/object/bbox/xmax': float_list_feature(xmaxs),
+        'image/object/bbox/ymin': float_list_feature(ymins),
+        'image/object/bbox/ymax': float_list_feature(ymaxs),
+        'image/object/class/text': bytes_list_feature(classes_text),
+        'image/object/class/label': int64_list_feature(classes),
     }))
     return tf_example
 
